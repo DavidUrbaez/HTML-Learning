b26329df485c1f331f97ed0798311a9089536867
DavidUrbaez <durbaezl@unal.edu.co>
2020-12-15 21:51:22 -0500

    455

--- SnakeGame/snake.js
+++ SnakeGame/snake.js
@@ -38,8 +38,11 @@ function Snake() {
             var pos = this.tail[i];
             var d = dist(this.x, this.y, pos.x, pos.y);
             if (d < 1) {
+                console.log('Perdiste!!!!');
+                console.log('Puntaje', this.total);
                 this.total = 0;
                 this.tail = [];
+
             }
         }
     }
